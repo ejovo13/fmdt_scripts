f035af0f85eccb746ade3d69c1ff128e823ad752
archstation <ejovo13@yahoo.com>
2023-02-02 07:25:48 +0100

    add example

--- split_video.py
+++ split_video.py
@@ -3,3 +3,7 @@ Script to read in the output of `fmdt-detect` (see https://github.com/alsoc/fmdt
 and split a video into small chunks where meteors have been detected
 
 """
+
+import fmdt
+
+fmdt.split_video_at_meteors("demo.mp4", "ex1_detect_tracks.txt")
