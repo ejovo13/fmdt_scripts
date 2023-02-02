2058c5c4016ada3661a22f29b1ab081ee4e0a62e
archstation <ejovo13@yahoo.com>
2023-02-02 09:57:36 +0100

    add necessary files to upload to pypi

--- demo.py
+++ demo.py
@@ -12,3 +12,16 @@ visu      = "demo_visu.mp4"
 fmdt.detect(in_video, tracks, bbs)
 fmdt.visu(in_video, tracks, bbs, visu)
 fmdt.split_video_at_meteors(visu, tracks)
+
+# TODO: Improve the api to save information between calls.
+# It would be really nice to take the above block of code and 
+# convert it into
+#
+# (
+#     fmdt
+#     .detect(in_video, tracks, bbs)
+#     .visu()
+#     .split()
+# )
+#
+#
