74c9196662423df18cb5e123d1f39e8d257b830a
archstation <ejovo13@yahoo.com>
2023-02-02 17:31:43 +0100

    fix imports

--- fmdt/__init__.py
+++ fmdt/__init__.py
@@ -1,2 +1,10 @@
-from fmdt.core import *
-from fmdt.api  import *
+from fmdt.core import (
+    extract_all_information,
+    extract_key_information,
+    split_video_at_meteors
+) 
+
+from fmdt.api  import (
+    detect,
+    visu
+)
